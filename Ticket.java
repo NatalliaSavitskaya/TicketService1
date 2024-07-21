648fd8fd5bf578329e1a6ce4da3228cda49388ab
Natallia Savitskaya <nn.savitskaya@gmail.com>
2024-07-21 15:44:11 +0300

    first version of variables types

---
+++ Ticket.java
@@ -0,0 +1,47 @@
+public class Ticket
+{
+    private char[] idTicket = new char[4];
+    private char[] concertHall = new char[10];
+    private int[] eventCode = new int[3];
+    private long unixTimeStamp;
+    private boolean isPromo;
+    private char stadiumSector;
+    private float maxWeight;
+
+    public Ticket() {
+    }
+
+    public Ticket(char[] concertHall, int[] eventCode, long unixTimeStamp){
+        this.concertHall = concertHall;
+        this.eventCode = eventCode;
+        this.unixTimeStamp = unixTimeStamp;
+    }
+
+    public Ticket(char[] idTicket, char[] concertHall, int[] eventCode, long unixTimeStamp, boolean isPromo, char stadiumSector, float maxWeight){
+        this.idTicket = idTicket;
+        this.concertHall =concertHall;
+        this.eventCode =eventCode;
+        this.unixTimeStamp = unixTimeStamp;
+        this.isPromo = isPromo;
+        this.stadiumSector = stadiumSector;
+        this.maxWeight = maxWeight;
+    }
+
+    public void printTicket(){
+        System.out.println("Ticket ID: " + new String(idTicket));
+        System.out.println("The concert hall is: " + new String(concertHall));
+        System.out.println("The event code is: " + mergeArrayToString(eventCode));
+        System.out.println("The ticket creation time is: " + unixTimeStamp);
+        System.out.println("This is a promo ticket: " + isPromo);
+        System.out.println("The stadium sector is: " + stadiumSector);
+        System.out.println("Max allowed backpack weight (kg.g): " + maxWeight);
+    }
+
+    public String mergeArrayToString(int[] arrayNumbers) {
+        StringBuilder sb = new StringBuilder();
+        for (int number : arrayNumbers) {
+            sb.append(number);
+        }
+        return sb.toString();
+    }
+}
---
+++ TicketService.java
@@ -0,0 +1,22 @@
+class TicketService
+{
+    public static void main(String []args)
+    {
+        Ticket emptyTicket = new Ticket();
+        System.out.println("The info about the empty ticket is:");
+        emptyTicket.printTicket();
+
+        char[] concertHall = {'1','1','1','1','1','1','1','1','1','1'};
+        int[] eventCode = {1,1,1};
+        long unixTimeStamp = System.currentTimeMillis()/1000L;
+        Ticket limitedTicket = new Ticket(concertHall, eventCode, unixTimeStamp);
+        System.out.println("The info about the limited ticket is:");
+        limitedTicket.printTicket();
+
+        char[] idTicket = {'1','1','1','1'};
+        Ticket fullTicket = new Ticket(idTicket, concertHall, eventCode, unixTimeStamp,
+                true, 'A', 55.95F);
+        System.out.println("The info about the full ticket is:");
+        fullTicket.printTicket();
+    }
+};
